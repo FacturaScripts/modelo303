506a6fecc1d712b42fecbf1bb15e037dd2bb1f90
Esteban Sánchez <estebansm85@gmail.com>
2026-06-02 12:29:34 +0200

    Refactorización de filtros en el plugin Modelo303
    https://facturascripts.com/roadmap/4709
    -Se actualizo todos los JoinModel para heredar de la nueva clase de Core/Template
    - Se reemplazaron instancias de DataBaseWhere por la nueva clase Where en varios archivos:
      - ComprobarFechaDevengoTest.php
      - EditRegularizacionImpuesto.php
      - ListRegularizacionImpuesto.php
      - PartidaImpuesto.php
      - PartidaImpuestoResumen.php
      - VatRegularizationToAccounting.php
    
    - Se actualizó la versión mínima requerida en facturascripts.ini a 2026.
    - Se corrigió la fecha de copyright en varios archivos a 2026.

--- Model/Join/PartidaImpuestoResumen.php
+++ Model/Join/PartidaImpuestoResumen.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of Modelo303 plugin for FacturaScripts
- * Copyright (C) 2017-2025 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2017-2026 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -18,7 +18,7 @@
  */
 namespace FacturaScripts\Plugins\Modelo303\Model\Join;
 
-use FacturaScripts\Core\Model\Base\JoinModel;
+use FacturaScripts\Core\Template\JoinModel;
 
 /**
  * Auxiliary model to load a resume of accounting entries with VAT
@@ -105,7 +105,7 @@ class PartidaImpuestoResumen extends JoinModel
      *
      * @param array $data
      */
-    protected function loadFromData(array $data)
+    protected function loadFromData(array $data): void
     {
         parent::loadFromData($data);
 
